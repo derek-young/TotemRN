b70374788275da131008fa07bbb57c373fe34802
Derek <dthomasy@gmail.com>
2017-03-31 19:29:18 +0800

    initial routes

--- app/App.js
+++ app/App.js
@@ -3,17 +3,26 @@ import {
   Text,
   View
 } from 'react-native';
-import Header from './components/Header/Header'
+
 import styles from './styles';
+
+/* Components */
+import Header from './components/Header/Header';
+import Group from './components/Group/Group';
+import MapViewer from './components/MapViewer/MapViewer';
 
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
         <Header />
-        <Text>
-          Hello World
-        </Text>
+        {/* <Route exact path="/" component={HomeView}/> */}
+        {/* <Route path="/map" component={MapViewer}/> */}
+        {/* <Route path="/group" component={Group}/> */}
+        {/* <Route path="/agenda" component={Agenda}/> */}
+        {/* <Route path="/schedule" component={VenueSchedule}/> */}
+        {/* <Route path="/choosevenue" component={ChooseVenue}/> */}
+        {/* <Route path="/creategroup" component={CreateGroup}/> */}
       </View>
     );
   }
---
+++ app/components/Group/Group.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+  Text,
+  View
+} from 'react-native';
+
+const Group = () => (
+  <View>
+    <Text>
+      Group View
+    </Text>
+  </View>
+);
+
+export default Group;
